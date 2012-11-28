ab309c3eeb2ed7b8171e15d09a5793573301358e
Gjert Aaberge <gjertaab@stud.ntnu.no>
2012-11-28 22:49:50 +0100

    Endra rettigheter i liste.php, pluss fnask i galleri

---
+++ sider/bilder/bilder.php
@@ -0,0 +1,26 @@
+
+
+<div class="album">
+<a href='?side=bilder/galleri/'><img src="sider/bilder/Himmel.jpg" alt="smedtekst" class="album-bilde ingressbilde"></a>
+<p class="album-text">galleri 1</p>
+</div>
+<div class="album">
+<a href='?side=bilder/galleri/'><img src="sider/bilder/Himmel.jpg" alt="smedtekst" class="album-bilde ingressbilde"></a> 
+<p class="album-text">galleri 2</p>
+</div>
+<div class="album">
+<a href='?side=bilder/galleri/'><img src="sider/bilder/Himmel.jpg" alt="smedtekst" class="album-bilde ingressbilde"></a>
+<p class="album-text">galleri 2</p>
+</div>
+<div class="album">
+<a href='?side=bilder/galleri/'><img src="sider/bilder/Himmel.jpg" alt="smedtekst" class="album-bilde ingressbilde"></a>
+<p class="album-text">galleri 3</p>
+</div>
+<div class="album">
+<a href='?side=bilder/galleri/'><img src="sider/bilder/Himmel.jpg" alt="smedtekst" class="album-bilde ingressbilde"></a>
+<p class="album-text">galleri 4</p>
+</div>
+<div class="album">
+<a href='?side=bilder/galleri/'><img src="sider/bilder/Himmel.jpg" alt="smedtekst" class="album-bilde ingressbilde"></a>
+<p class="album-text">galleri 5</p>
+</div>
---
+++ sider/bilder/galleri.php
@@ -0,0 +1,45 @@
+
+
+
+<div class="album galleri">
+	<img src="sider/bilder/Himmel.jpg" alt="smedtext" class="album galleri">
+</div>
+<br>
+<div class="album liten">
+<img src="sider/bilder/Himmel.jpg" alt="smedtekst" class="album-bilde liten">
+</div>
+<div class="album liten">
+<img src="sider/bilder/Himmel.jpg" alt="smedtekst" class="album-bilde liten">
+</div>
+<div class="album liten">
+<img src="sider/bilder/Himmel.jpg" alt="smedtekst" class="album-bilde liten">
+</div>
+<div class="album liten">
+<img src="sider/bilder/Himmel.jpg" alt="smedtekst" class="album-bilde liten">
+</div>
+<div class="album liten">
+<img src="sider/bilder/Himmel.jpg" alt="smedtekst" class="album-bilde liten">
+</div>
+<div class="album liten">
+<img src="sider/bilder/Himmel.jpg" alt="smedtekst" class="album-bilde liten">
+</div>
+	
+<br>
+<hr>
+<div class="album middels">
+<img src="sider/bilder/Himmel.jpg" alt="smedtekst" class="album-bilde middels">
+</div>
+<div class="album middels">
+<img src="sider/bilder/Himmel.jpg" alt="smedtekst" class="album-bilde middels">
+</div>
+<div class="album middels">
+<img src="sider/bilder/Himmel.jpg" alt="smedtekst" class="album-bilde middels">
+</div>
+<div class="album middels">
+<img src="sider/bilder/Himmel.jpg" alt="smedtekst" class="album-bilde middels">
+</div>
+<div class="album middels">
+<img src="sider/bilder/Himmel.jpg" alt="smedtekst" class="album-bilde middels">
+</div>
+
+	
