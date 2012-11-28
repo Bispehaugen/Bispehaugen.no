32873444f91fb0f45d1fb252b933602aeae1098e
Trond Klakken <trondkla@stud.ntnu.no>
2012-11-28 19:19:38 +0100

    Fil for å koble til database. Lag en lokal_config.php som er lik eksemplet i filen for å bruke

---
+++ db_config.php
@@ -0,0 +1,15 @@
+<?php
+
+if(file_exists("lokal_config.php")){
+	include("lokal_config.php");
+}
+
+/*
+
+Slik ser lokal_config.php ut: 
+<?php
+	$database_host="localhost";
+	$database_user="solfrih_bukdb";
+	$database_string="passord her";
+	$database_database = "solfrih_bukdb";
+
