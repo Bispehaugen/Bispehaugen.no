46f716d5e10b4f3276a377ef4114122f0fada378
Trond Klakken <trondkla@stud.ntnu.no>
2013-01-16 20:31:39 +0100

    Mer oversiktlig kode på informasjonskolonne

--- sider/informasjonskolonne.php
+++ sider/informasjonskolonne.php
@@ -4,40 +4,37 @@
 ## Neste konsert
 ###############
 setlocale(LC_TIME, "norwegian"); 
-	$sql3="SELECT * FROM nyheter WHERE type='nestekonsert' AND aktiv='1' ORDER BY tid DESC";
-	$sql3_result=mysql_query($sql3);
 
-	if ($row=mysql_fetch_array($sql3_result)) {
-		$id = $row["nyhetsid"];
-		$bilde=$row["bilde"];
-		$nyhetsid=$row["nyhetsid"];
-		$overskrift=$row["overskrift"];		
-		$aktiv=$row["aktiv"];
-		$konsert_tid = $row["konsert_tid"];
-		$normal_pris = $row["normal_pris"];
-		$student_pris = $row["student_pris"];
-		$sted = $row["sted"];
-	}
-	
+$konsert = hent_og_putt_inn_i_array("SELECT * FROM nyheter WHERE type='nestekonsert' AND aktiv='1' ORDER BY tid DESC");
+
+// Bare vis hvis det fins en aktiv neste konsert
+if (!empty($konsert)) {
+
+	$bilde = $konsert['bilde'];
 	if(empty($bilde)){
 		$bilde = "bilder/forside/logo.png";
 	}
+	?>
+	<div class="news">
+	    <div class="next_concert">
+	    	<h3 class="next_concert"><a class="next_concert" href="?side=nyhet&id=$id" title="Neste konsert">Neste konsert</a></h3>	
+	        <img src="<?php echo $bilde; ?>" />
+	        <h4><?php echo $konsert['overskrift']; ?></h4>
+	        <div class="info">
+	        <b>Tid:</b> <?php echo ucfirst(strftime("%A %#d. %B", strtotime($konsert['$konsert_tid']))); ?>, kl. <?php 
+	        echo strftime("%H:%M", strtotime($konsert['$konsert_tid'])); ?><br />
+	        <b>Pris:</b> <?php echo $konsert['normal_pris']; ?>/<?php echo $konsert['student_pris']; ?><br />
+	        <b>Sted:</b> <?php echo $konsert['sted']; ?>
+	        </div>
+	        <br />
+	        <div class="read_more"><a href="">Les mer</a></div>
+	        
+	    </div>
+	</div>
+
+<?php 
+}
 ?>
-<div class="news">
-    <div class="next_concert">
-    	<h3 class="next_concert"><a class="next_concert" href="?side=nyhet&id=$id" title="Neste konsert">Neste konsert</a></h3>	
-        <img src="<?php echo $bilde; ?>" />
-        <h4><?php echo $overskrift; ?></h4>
-        <div class="info">
-        <b>Tid:</b> <?php echo ucfirst(strftime("%A %#d. %B", strtotime($konsert_tid))); ?>, kl. <?php echo strftime("%H:%M", strtotime($konsert_tid)); ?><br />
-        <b>Pris:</b> <?php echo $normal_pris; ?>/<?php echo $student_pris; ?><br />
-        <b>Sted:</b> <?php echo $sted; ?>
-        </div>
-        <br />
-        <div class="read_more"><a href="">Les mer</a></div>
-        
-    </div>
-</div>
 
 <div class="advertisement">
 	<div class="grasrota">
