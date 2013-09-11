d8e8342e54962d4ab2dbe898a67f068953fbc6db
trondkla <trond.klakken@bekk.no>
2013-09-11 20:28:23 +0200

    Diverse fikser

--- sider/forside.php
+++ sider/forside.php
@@ -1,10 +1,9 @@
 <?php
 
-$siste_nyheter = hent_siste_nyheter(3);
+$siste_nyheter = hent_siste_nyheter(6);
 
-//TODO: her skal vi hente ut siste 3 ikke-aktive så spørringa må endres
-$siste_konserter = hent_eldre_konserter(3);
 
+if (!er_logget_inn()) {
 ?>
 <a href="?side=spilleoppdrag/spilleoppdrag"><h1>Vi stiller med musikalske innslag!</h1><a/>
 <div class="banner_container">
@@ -36,34 +35,8 @@ $siste_konserter = hent_eldre_konserter(3);
 	
 </div>
 <?php
-echo" 
-<div class='lastest_concerts'>
-    	<h2><a href='' title='De siste konsertene'>Siste konserter</a></h2>
-";
-
-foreach($siste_konserter as $konsert){
-	
-	$bilde = $konsert['bilde'];
-	//if(empty($bilde)){
-		$bilde = "bilder/forside/logo.png";
-	//}
-	
-echo '
-	
-		<div class="news">
-	    	<div class="image"><img src="'.$bilde.'" /></div>
-	    	<h4>'.$konsert['overskrift'].'</h4>
-	    	<p>'.$konsert['ingress'].'</p>
-	    	<div class="date">
-			    '.date("d. M", strtotime($konsert['tid'])).' kl.'.date("H:m", strtotime($konsert['tid'])).'
-		    	<div class="read_more"><a href="?side=nyhet&id='.$konsert['nyhetsid'].'">Les mer</a></div>
-	    	</div>
-		</div>
-	';
 }
-echo"</div>"
 ?>
-
 <h2 class="news"><a class="news" href="?side=nyhet" title="Les flere nyheter">Nyheter</a></h2>
 <?php
 foreach($siste_nyheter as $nyhet){
