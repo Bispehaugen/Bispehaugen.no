048c7e14d6908c72465842521933b99945e2dc0a
Trond Klakken <trondkla@stud.ntnu.no>
2013-10-04 22:59:55 +0200

    Starten på en prøveting

---
+++ demo/index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang='no' xml:lang='no' xmlns='http://www.w3.org/1999/xhtml'>
+  <head>
+    <title>Bispehaugen Ungdomskorps</title>
+    <script src='jquery-1.10.2.min.js' type='text/javascript'></script>
+    <script src='scrollIt.js' type='text/javascript'></script>
+    <script src='unslider.min.js' type='text/javascript'></script>
+    <link href='style.css' rel='stylesheet'>
+  </head>
+  <body>
+    <div class='nav-container'>
+      <nav>
+        <ul>
+          <li>
+            <a class='active' data-scroll-nav='0'>Nyheter</a>
+          </li>
+          <li>
+            <a data-scroll-nav='1'>Aktiviteter</a>
+          </li>
+          <li>
+            <a data-scroll-nav='2'>Spilleoppdrag</a>
+          </li>
+          <li>
+            <a data-scroll-nav='3'>Bli medlem!</a>
+          </li>
+          <li>
+            <a data-scroll-nav='4'>Medlemmer</a>
+          </li>
+          <li>
+            <a data-scroll-nav='5'>Korpset</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <section class="nyheter" data-scroll-index='0'>
+      <div class='content'>
+        <div class="box news">
+          <div class="image">
+            <img src="../bilder/forside/logo.png">
+          </div>
+          <div class="innhold">
+            <h4>Kunne du tenke deg å bli støttemedlem?</h4>
+            <p>Ønsker du å støtte Bispehaugen Ungdomskorps? Hva med å bli støttemedlem? Dette er en ordning vi har hvor venner, familie og bekjente kan støtte oss med enten 300, 600 eller 900 kr i året.</p>
+            <div class="date">
+              06. May kl.23:05
+              <div class="read_more"><a href="?side=nyhet&amp;id=1373">Les mer</a></div>
+            </div>
+          </div>
+        </div>
+
+        <div class="box news">
+          <div class="image">
+            <img src="../bilder/forside/logo.png">
+          </div>
+          <div class="innhold">
+            <h4>Kunne du tenke deg å bli støttemedlem?</h4>
+            <p>Ønsker du å støtte Bispehaugen Ungdomskorps? Hva med å bli støttemedlem? Dette er en ordning vi har hvor venner, familie og bekjente kan støtte oss med enten 300, 600 eller 900 kr i året.</p>
+            <div class="date">
+              06. May kl.23:05
+              <div class="read_more"><a href="?side=nyhet&amp;id=1373">Les mer</a></div>
+            </div>
+          </div>
+        </div>
+
+                <div class="box news">
+          <div class="image">
+            <img src="../bilder/forside/logo.png">
+          </div>
+          <div class="innhold">
+            <h4>Kunne du tenke deg å bli støttemedlem?</h4>
+            <p>Ønsker du å støtte Bispehaugen Ungdomskorps? Hva med å bli støttemedlem? Dette er en ordning vi har hvor venner, familie og bekjente kan støtte oss med enten 300, 600 eller 900 kr i året.</p>
+            <div class="date">
+              06. May kl.23:05
+              <div class="read_more"><a href="?side=nyhet&amp;id=1373">Les mer</a></div>
+            </div>
+          </div>
+        </div>
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                <div class="box news">
+          <div class="image">
+            <img src="../bilder/forside/logo.png">
+          </div>
+          <div class="innhold">
+            <h4>Kunne du tenke deg å bli støttemedlem?</h4>
+            <p>Ønsker du å støtte Bispehaugen Ungdomskorps? Hva med å bli støttemedlem? Dette er en ordning vi har hvor venner, familie og bekjente kan støtte oss med enten 300, 600 eller 900 kr i året.</p>
+            <div class="date">
+              06. May kl.23:05
+              <div class="read_more"><a href="?side=nyhet&amp;id=1373">Les mer</a></div>
+            </div>
+          </div>
+        </div>
+
+        <div class="box news">
+          <div class="image">
+            <img src="../bilder/forside/logo.png">
+          </div>
+          <div class="innhold">
+            <h4>Kunne du tenke deg å bli støttemedlem?</h4>
+            <p>Ønsker du å støtte Bispehaugen Ungdomskorps? Hva med å bli støttemedlem? Dette er en ordning vi har hvor venner, familie og bekjente kan støtte oss med enten 300, 600 eller 900 kr i året.</p>
+            <div class="date">
+              06. May kl.23:05
+              <div class="read_more"><a href="?side=nyhet&amp;id=1373">Les mer</a></div>
+            </div>
+          </div>
+        </div>
+
+        <div class="box news">
+          <div class="image">
+            <img src="../bilder/forside/logo.png">
+          </div>
+          <div class="innhold">
+            <h4>Kunne du tenke deg å bli støttemedlem?</h4>
+            <p>Ønsker du å støtte Bispehaugen Ungdomskorps? Hva med å bli støttemedlem? Dette er en ordning vi har hvor venner, familie og bekjente kan støtte oss med enten 300, 600 eller 900 kr i året.</p>
+            <div class="date">
+              06. May kl.23:05
+              <div class="read_more"><a href="?side=nyhet&amp;id=1373">Les mer</a></div>
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
+    </section>
+    <section class="aktiviteter" data-scroll-index='1'>
+      <div class='content'>
+        <h2>Aktiviteter</h2>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+        <p>Masse masse</p>
+      </div>
+    </section>
+    <section class="spilleoppdrag" data-scroll-index='2'>
+      <div class='content'>
+        <h2>Spilleoppdrag</h2>
+
+      </div>
+    </section>
+    <section class="blimedlem" data-scroll-index='3'>
+      <div class='content'>
+        <h2>Bli medlem!</h2>
+
+      </div>
+    </section>
+    <section class="medlemmer" data-scroll-index='4'>
+      <div class='content'>
+        <h2>Medlemmer</h2>
+
+      </div>
+    </section>
+    <section class="korpset" data-scroll-index='5'>
+      <div class='content'>
+        <h2>Korpset</h2>
+
+      </div>
+    </section>
+
+    <script>
+      $.scrollIt();
+
+      //$(".aktiviteter").height($(".aktiviteter .content").outerHeight(true));
+
+      var unslider = $('.banner').unslider();
+  
+      $('.unslider-arrow').click(function() {
+          var fn = this.className.split(' ')[1];
+          
+          //  Either do unslider.data('unslider').next() or .prev() depending on the className
+          unslider.data('unslider')[fn]();
+      });
+    </script>
+  </body>
+</html>
