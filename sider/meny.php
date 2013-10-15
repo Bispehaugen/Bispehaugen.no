61dd8f0c6a69401beebcbbbedfedf5ec9f0ee463
Anne-Siri Borander <as.borander@gmail.com>
2013-10-15 21:52:44 +0200

    oppdatert link til noter i menyen

--- sider/meny.php
+++ sider/meny.php
@@ -7,7 +7,7 @@ if(er_logget_inn()){
     <li><a href="?side=aktiviteter/liste">Aktiviteter</a></li>
     <li><a href="?side=bilder/bilder">Bilder</a></li>
     <li><a href="?side=dokumenter">Dokumenter</a></li>
-    <li><a href="?side=noter">Noter</a></li>
+    <li><a href="?side=noter/noter_oversikt">Noter</a></li>
     <li><a href="?side=medlem/liste">Medlemmer</a></li>
     <li><a href="?side=forum/forum">Forum</a></li>
     <li><a href="?side=organisasjon">Organisasjonen</a></li>
