c80a97493db89607b17db73a9763a77faef03ed8
Trond Klakken <trondkla@stud.ntnu.no>
2013-10-29 21:35:56 +0100

    Lagt til mer på demo :)

--- demo/index.php
+++ demo/index.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
-<html lang='no' xml:lang='no' xmlns='http://www.w3.org/1999/xhtml'>
+<html lang='no' xml:lang='no' charset="utf-8" xmlns='http://www.w3.org/1999/xhtml'>
   <head>
     <title>Bispehaugen Ungdomskorps</title>
     <script src='jquery-1.10.2.min.js' type='text/javascript'></script>
     <script src='scrollIt.js' type='text/javascript'></script>
     <script src='unslider.min.js' type='text/javascript'></script>
+    <link href='http://fonts.googleapis.com/css?family=Dosis:300,400,500&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Nunito:400,700,300' rel='stylesheet' type='text/css'>
     <link href='style.css' rel='stylesheet'>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   </head>
   <body>
+    <section class="forside">
+      <h1 class="title">Bispehaugen Ungdomskorps</h1>
+      
+    </section>
     <div class='nav-container'>
       <nav>
         <ul>
@@ -21,7 +28,7 @@
             <a data-scroll-nav='2'>Spilleoppdrag</a>
           </li>
           <li>
-            <a data-scroll-nav='3'>Bli medlem!</a>
+            <a class="bli-medlem" data-scroll-nav='3'>Bli medlem</a>
           </li>
           <li>
             <a data-scroll-nav='4'>Medlemmer</a>
@@ -33,7 +40,7 @@
       </nav>
     </div>
     <section class="nyheter" data-scroll-index='0'>
-      <div class='content'>
+    <div class='content'>
         <div class="box news">
           <div class="image">
             <img src="../bilder/forside/logo.png">
@@ -139,6 +146,248 @@
     <section class="aktiviteter" data-scroll-index='1'>
       <div class='content'>
         <h2>Aktiviteter</h2>
+
+          <table cellspacing=0 style="width: 100%">
+  <tr style="text-align: left">
+    <th colspan=2 width="22%"><b>Dato</th>
+    <th width="35%"><b>Arrangement</th>
+    <th width="30%"><b>Sted</th>
+    <th width="10%"><b>Starter</th>
+    <th width="0">&nbsp;</th>
+  </tr>
+  <tr>
+    <td>fre</td>
+    <td>01. nov</td>
+    <td>Seminar</td>
+    <td>Bispehaugen Skole</td>
+    <td>18:00</td>
+  
+    <td>&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>lør</td>
+    <td>02. nov</td>
+    <td>Seminar</td>
+    <td>Bispehaugen Skole</td>
+    <td>10:00</td>
+  
+    <td>&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>søn</td>
+    <td>03. nov</td>
+    <td>Seminar</td>
+    <td>Bispehaugen Skole</td>
+    <td>12:00</td>
+  
+    <td>&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>man</td>
+    <td>04. nov</td>
+    <td>Øvelse</td>
+    <td>Bispehaugen Skole</td>
+    <td>19:30</td>
+  
+    <td>&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>man</td>
+    <td>11. nov</td>
+    <td>Øvelse</td>
+    <td>Bispehaugen Skole</td>
+    <td>19:30</td>
+  
+    <td>&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>fre</td>
+    <td>15. nov</td>
+    <td>Jubileumskonsert</td>
+    <td>Trondheim Spektrum</td>
+    <td>09:00</td>
+      
+    <td class=small_text><href="?id=989&action=vis">&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>man</td>
+    <td>18. nov</td>
+    <td>Øvelse</td>
+    <td>Bispehaugen Skole</td>
+    <td>19:30</td>
+  
+    <td>&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>man</td>
+    <td>25. nov</td>
+    <td>Øvelse</td>
+    <td>Bispehaugen Skole</td>
+    <td>19:30</td>
+  
+    <td>&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>søn</td>
+    <td>01. des</td>
+    <td>Adventskonsert</td>
+    <td>Rosenborg Skole</td>
+    <td>&nbsp;</td>
+      
+    <td class=small_text><href="?id=975&action=vis">&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>man</td>
+    <td>02. des</td>
+    <td>Øvelse</td>
+    <td>Bispehaugen Skole</td>
+    <td>19:30</td>
+  
+    <td>&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>man</td>
+    <td>09. des</td>
+    <td>Øvelse</td>
+    <td>Bispehaugen Skole</td>
+    <td>19:30</td>
+  
+    <td>&nbsp;</td>
+    
+  </tr>
+
+  <tr>
+    <td>man</td>
+    <td>06. jan</td>
+    <td>Første øvelse jul</td>
+    <td>Bispehaugen Skole</td>
+    <td>19:30</td>
+  
+    <td>&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>fre</td>
+    <td>17. jan</td>
+    <td>Prøvekorps dirigentsymposiumet</td>
+    <td>Hell</td>
+    <td>&nbsp;</td>
+  
+    <td>&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>lør</td>
+    <td>18. jan</td>
+    <td>Prøvekorps dirigentsymposiumet</td>
+    <td>Hell</td>
+    <td>&nbsp;</td>
+  
+    <td>&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>søn</td>
+    <td>19. jan</td>
+    <td>Prøvekorps dirigentsymposiumet</td>
+    <td>Hell</td>
+    <td>&nbsp;</td>
+  
+    <td>&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>søn</td>
+    <td>02. feb</td>
+    <td>Trøndersk Mesterskap</td>
+    <td>Olavshallen, store sal</td>
+    <td>&nbsp;</td>
+      
+    <td class=small_text><href="?id=1009&action=vis">&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>fre</td>
+    <td>04. apr</td>
+    <td>NM 2014</td>
+    <td>Trondheim</td>
+    <td>&nbsp;</td>
+      
+    <td class=small_text><href="?id=1010&action=vis">&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>lør</td>
+    <td>05. apr</td>
+    <td>NM 2014</td>
+    <td>Trondheim</td>
+    <td>&nbsp;</td>
+      
+    <td class=small_text><href="?id=1011&action=vis">&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>tor</td>
+    <td>01. mai</td>
+    <td>1.Mai</td>
+    <td>Trondheim</td>
+    <td>&nbsp;</td>
+      
+    <td class=small_text><href="?id=1012&action=vis">&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td>lør</td>
+    <td>17. mai</td>
+    <td>17.Mai</td>
+    <td>Trondheim</td>
+    <td>&nbsp;</td>
+      
+    <td class=small_text><href="?id=1013&action=vis">&nbsp;</td>
+    
+  </tr>
+  
+  <tr>
+    <td></td>
+  </tr>
+  </table>
+      </div>
+    </section>
+    <section class="spilleoppdrag" data-scroll-index='2'>
+     
+      <div class='content'>
+        <h2>Spilleoppdrag</h2>
+
+      <div class='content'>
+        <h2>Aktiviteter</h2>
         <p>Masse masse</p>
         <p>Masse masse</p>
         <p>Masse masse</p>
@@ -179,11 +428,6 @@
         <p>Masse masse</p>
         <p>Masse masse</p>
       </div>
-    </section>
-    <section class="spilleoppdrag" data-scroll-index='2'>
-      <div class='content'>
-        <h2>Spilleoppdrag</h2>
-
       </div>
     </section>
     <section class="blimedlem" data-scroll-index='3'>
