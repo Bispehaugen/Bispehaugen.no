813e428754cd4389016e9d83093322281435db72
Anne-Siri Borander <as.borander@gmail.com>
2014-01-12 22:19:39 +0100

    prøvde å fikse slette-knapp for aktiviteter

--- sider/aktiviteter/slette.php
+++ sider/aktiviteter/slette.php
@@ -3,14 +3,14 @@
 	//funksjonalitet
 	
 	//sjekker om man er admin
-	if($_SESSION['rettigheter']<2){
+	if($_SESSION['rettigheter']>1){
 		header('Location: ?side=aktiviteter/liste');
 	};
 	
 	$id=get('id');
 	
 	if(has('id')){
-		//$sql = "UPDATE arrangement SET slettet=true WHERE arrid = '".$id."';";
+		$sql = "UPDATE arrangement SET slettet=true WHERE arrid = '".$id."';";
 		mysql_query($sql);
 		header('Location: ?side=aktiviteter/liste');
 		exit('tets');
