52049b873ab777d2eb894a2d899e8b93b20d69bd
Anne-Siri Borander <as.borander@gmail.com>
2014-01-12 22:20:25 +0100

    lagt inn ikon for endre/slette-ikon og når de vises

--- sider/forum/innlegg.php
+++ sider/forum/innlegg.php
@@ -30,12 +30,12 @@
 		mysql_query($sql);
 	};
 	
-	//hvis noen har skrevet seg pï¿½ en liste
+	//hvis noen har skrevet seg på en liste
 	if(has_post('listeinnlegg')){
 		//fjerner alt skummelt fra kommentarfeltet og setter inn feltet
 		$kommentar=post('kommentar');
 		if(post('flagg')==1){$flagg=1;}else{$flagg=0;};
-		//sql - databasen er sï¿½nn at pdd. kan du ikke melde deg pï¿½ nï¿½r du allerede er pï¿½meldt
+		//sql - databasen er sånn at pdd. kan du ikke melde deg på når du allerede er påmeldt
 		$sql="INSERT INTO forum_listeinnlegg_ny (listeid, flagg, brukerid, kommentar, tid) 
 			VALUES ('".post('listeinnlegg')."','".$flagg."','".$_SESSION["medlemsid"]."','".$kommentar."','".date('Y-m-d h:i:s')."')";
 		mysql_query($sql);	
@@ -53,7 +53,7 @@
 		WHERE forum_liste.listeid=forum_innlegg_ny.innleggid;";
 	$listeinnlegg=hent_og_putt_inn_i_array($sql, "listeid");	
 	
-	//henter ut alle aktuelle liste-oppfï¿½ringer
+	//henter ut alle aktuelle liste-oppfåringer
 	$sql="SELECT medlemsid, fnavn, enavn, forum_listeinnlegg_ny.listeid, forum_listeinnlegg_ny.tid, forum_listeinnlegg_ny.innleggid, 
 	forum_listeinnlegg_ny.kommentar, forum_listeinnlegg_ny.flagg, forum_liste.expires 
 	FROM forum_liste, forum_innlegg_ny, forum_listeinnlegg_ny, forum_tema, medlemmer 
@@ -61,7 +61,7 @@
 	 forum_tema.temaid=".$temaid." AND forum_innlegg_ny.temaid=".$temaid." AND brukerid=medlemmer.medlemsid ORDER BY tid ;";
 	$listeoppforinger=hent_og_putt_inn_i_array($sql, "innleggid");	
 		
-	//Henter ut siste uleste innlegg i trï¿½d
+	//Henter ut siste uleste innlegg i tråd
 	$medlemsid= $_SESSION["medlemsid"];
 	$sql="SELECT uleste_innlegg FROM forum_leste WHERE temaid=".$temaid." AND medlemsid=".$medlemsid.";";
 	$result = mysql_query($sql);
@@ -76,7 +76,7 @@
 	$sql = "DELETE FROM `forum_leste` WHERE temaid=".$temaid." AND medlemsid=".$medlemsid.";";
 	mysql_query($sql);
 	
-    #Det som printes pï¿½ sida
+    #Det som printes på sida
     
     //Her legges det inn en oversikt over alle forumene
     list_forum();
@@ -94,7 +94,7 @@
 		echo "<td class='liten_tekst'>".strftime("%a %d. %b", strtotime($forum_innlegg['skrevet']))." skrev ".$forum_innlegg['fnavn']." ".$forum_innlegg['enavn']." </td>
    			<td>".$forum_innlegg['tekst'];
 		
-      	//if som skriver ut liste hvis det hï¿½rer en liste til innlegget
+      	//if som skriver ut liste hvis det hører en liste til innlegget
 		if($listeinnlegg[$forum_innlegg['innleggid']]){
 			echo "<table>
 			<tr><th colspan='2'>".$listeinnlegg[$forum_innlegg['innleggid']]['tittel']."</th></tr>";
@@ -111,7 +111,7 @@
 					if($listeoppforing['medlemsid']==$_SESSION["medlemsid"]){$oppfort_paa_liste=1;}	
 				};	
 			};
-			//Legger til tekstfelt for ï¿½ melde seg pï¿½ hvis ikke lista har expired
+			//Legger til tekstfelt for å melde seg på hvis ikke lista har expired
 			
 			if($oppfort_paa_liste==1){
 				echo "<tr><td colspan='2'><b>Du er allerede skrevet på lista</b></td></tr> ";						
@@ -120,13 +120,31 @@
 				<tr><td>Kommentar (frivillig):<br><input type='text' name='kommentar' autofocus><br><input type='checkbox' name='flagg' value='1'> Stryk navnet</td>
 				<td><input type='hidden' name='medlemsid' value='".$_SESSION['medlemsid']."'>
 				<input type='hidden' name='listeinnlegg' value='".$listeinnlegg[$forum_innlegg['innleggid']]['listeid']."'>
-				<input type='submit' name='nyttListeInnlegg' value='Skriv meg pï¿½ lista'></td></tr>";
+				<input type='submit' name='nyttListeInnlegg' value='Skriv meg på lista'></td></tr>";
 			}else{
-				echo "<tr><td colspan='2'><b>Det er ikke lenger mulig ï¿½ melde seg pï¿½ denne lista</b></td></tr> ";	
+				echo "<tr><td colspan='2'><b>Det er ikke lenger mulig å melde seg på denne lista</b></td></tr> ";	
 			};
 			echo "</form></table>";
   		};
-  		echo "</td><td><a href''>liker</a></td></tr>";
+			//legger til liker-ikon med antall likes (vises ikke for lister)
+			if($listeoppforing['listeid']!=$forum_innlegg['innleggid']){
+				echo"</td><td><i class='icon-thumbs-up' title='Antall som liker dette'>XX personer liker dette 
+				<br>";
+				//du kan bare like andres innlegg
+				if($forum_innlegg['skrevetavid']!=$_SESSION['medlemsid']){
+					echo"<br><a href='#'>Lik dette</i></a>";
+				};
+  			}else{
+  				echo"<td></td>";
+  			};
+			
+			//viser endre/slett-knapper på egne innlegg og at admin har mulighet til å overstyre
+			if($forum_innlegg['skrevetavid']==$_SESSION['medlemsid'] || $_SESSION['rettigheter']>1){
+  				echo"<br><a href='?side=forum/innlegg&id=".$temaid."&sletteinnlegg=".$forum_innlegg['innleggid']."'>
+  				<i class='icon-remove' title='Klikk for å slette'></i></a> / 
+  				<i class='icon-edit' title='Klikk for å endre (fungerer ikke ennå)'></i>";
+			};
+  			echo "</td></tr>";
 	};	
 	echo "
 	<form class='forum' method='post' action='?side=forum/innlegg&id=".$temaid."'>
