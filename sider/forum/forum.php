25878f93e24b7434be1c540ff2efe12a6ddd6e7b
Anne-Siri Borander <as.borander@gmail.com>
2014-01-12 19:49:01 +0100

    endret til hent_og_putt_inn_i_array() og så uleste også vises på forum-nivå

--- sider/forum/forum.php
+++ sider/forum/forum.php
@@ -14,12 +14,12 @@
 	
 	$sql="SELECT tittel, forum.forumid, pos, sisteinnleggskrevet, sisteinnleggskrevetav
 	FROM forum ORDER BY forumid;";
-	$mysql_result=mysql_query($sql);
-	$forum = Array();
-
-	while($row=mysql_fetch_array($mysql_result)){
-    	$forumer[$row['forumid']] = $row;
-	};
+	$forumer = hent_og_putt_inn_i_array($sql, "forumid");
+	
+	//henter forumid til forum som har uleste innlegg
+	$medlemsid= $_SESSION["medlemsid"];
+	$sql="SELECT forum_tema.forumid FROM forum_leste, forum_tema WHERE medlemsid=".$medlemsid." AND forum_leste.temaid=forum_tema.temaid;";
+	$uleste_forum = hent_og_putt_inn_i_array($sql, $id_verdi="forumid");
 	
     #Det som printes p� sida
     
@@ -29,12 +29,15 @@
     echo "<table class='forum'><tr><th></th><th>Forum</th><th>Sist oppdatert av</th></tr>";
   
    	//skriver ut alle forumene samt hvem som la inn siste innlegg og hvor lenge siden.
-   	//TODO: skal også sjekke om siste gang man sjekket forumet var før siste innlegg
    	foreach($forumer as $forum){
    		//sjekker om man er admin og dermed skal se styret, webkom og musikkomite-forumene
    		if($_SESSION['rettigheter']>2 || $forum['forumid']<3){
-   			//dager siden siste innlegg
-   			echo "<tr><td></td><td><a href='?side=forum/tema&id=".$forum['forumid']."'>".$forum['tittel']."</a></td><td>
+   			if($uleste_forum[$forum['forumid']]){
+   				echo "<tr class='ulest'>";
+			}else{
+				echo "<tr>";
+			};
+			echo"<td></td><td><a href='?side=forum/tema&id=".$forum['forumid']."'>".$forum['tittel']."</a></td><td>
    			".$forum['sisteinnleggskrevetav']." - ";
    			echo ant_dager_siden($forum['sisteinnleggskrevet'])."</td></tr>";
 		};
