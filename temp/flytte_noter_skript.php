07c0856bb108a27c35fcaccce95a8f9fa00d09f0
Anne-Siri Borander <as.borander@gmail.com>
2014-04-27 15:42:03 +0200

    sletter alt fordi logikken var feil

--- temp/flytte_noter_skript.php
+++ temp/flytte_noter_skript.php
@@ -16,25 +16,7 @@ function clean($string) {
 
    return preg_replace('/-+/', '-', $string); // Replaces multiple hyphens with single one.
 }
-$sql="SELECT * FROM lenker WHERE type='dir' AND katalog=381;";
-$notemapper=hent_og_putt_inn_i_array($sql, 'id');
-foreach($notemapper as $notemappe){
-	#echo $notemappe['tittel'];
-	$sql="SELECT tittel, id FROM lenker WHERE type='dir' AND katalog=".$notemappe['id'].";";
-	$undermapper=hent_og_putt_inn_i_array($sql, 'id');
-	$foldertittel=clean($notemappe['tittel']);
-		foreach($undermapper as $undermappe){
-			$sql="SELECT tittel, id FROM lenker WHERE type='dir' AND katalog=".$undermappe['id'].";";
-			$underundermapper=hent_og_putt_inn_i_array($sql, 'id');
-			$undertittel=clean($undermappe['tittel']);
-			#echo "<pre>".shell_exec($command)."</pre>";
 
-			foreach($underundermapper as $underundermappe){
-				$tittel=clean($underundermappe['tittel']);
-				$command="mkdir ".escapeshellarg("../noter/".$foldertittel."/".$undertittel."/".$tittel);
-				#print_r($command);
+				$command="rm -rf ../noter/";
 				echo "<pre>".shell_exec($command)."</pre>";
-			};
-		};
-};
 ?>
