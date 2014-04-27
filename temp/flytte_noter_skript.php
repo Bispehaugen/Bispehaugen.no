6e66a4b1bf05615767ee09c21de11787ca30417d
Anne-Siri Borander <as.borander@gmail.com>
2014-04-27 14:00:22 +0200

    lagt til riktig path til db-config

--- temp/flytte_noter_skript.php
+++ temp/flytte_noter_skript.php
@@ -2,19 +2,18 @@
 
 // Fjern denne linja
 setlocale(LC_TIME, "Norwegian");
-include_once "db_config.php";
-include_once "funksjoner.php";
-
+include_once "../db_config.php";
+include_once "../funksjoner.php";
 $tilkobling = koble_til_database($database_host, $database_user, $database_string, $database_database);
 
 if ($tilkobling === false) {
 	exit ;
 }
 
-$sql="SELECT * FROM lenker WHERE  `type` =  'dir' AND  `katalog` =381";
-$notemapper=hent_og_putt_inn_i_array($sql, "id");
+$sql="SELECT * FROM lenker WHERE type='dir' AND katalog=381;";
+$notemapper=hent_og_putt_inn_i_array($sql, 'id');
 foreach($notemapper as $notemappe){
 	$command="mkdir ../noter/".$notemappe['tittel'];
 	echo "<pre>".shell_exec($command)."</pre>";
-}
+};
 ?>
