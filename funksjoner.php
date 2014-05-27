d8d048921053d3848e77aa24a0ea795217a7d5b7
Anne-Siri Borander <as.borander@gmail.com>
2014-05-27 20:15:09 +0200

    lagt inn clean-funksjon (fjerner mellomrom og special characters)

--- funksjoner.php
+++ funksjoner.php
@@ -306,4 +306,11 @@ function dato($format, $tid) {
 	return date($format, strtotime($tid));
 }
 
+function clean($string) {
+   $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+   $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+
+   return preg_replace('/-+/', '-', $string); // Replaces multiple hyphens with single one.
+}
+
 ?>
