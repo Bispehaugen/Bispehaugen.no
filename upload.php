9dfc5160974b1a248d2a76883b43de3fe87f028d
Trond Klakken <trondkla@stud.ntnu.no>
2014-08-05 23:30:04 +0200

    Mer debugging info rundt filopplasting

--- upload.php
+++ upload.php
@@ -13,6 +13,8 @@ $config = new \Flow\Config(array(
 ));
 $request = new \Flow\Request();
 
+print_r($_REQUEST);
+
 $type = $_REQUEST['type'];
 $id = $_REQUEST['id'];
 
@@ -45,13 +47,16 @@ $filid = $id."-";
 $filename = substr($filid . $name, 0, 251);
 
 if ($type == "profilbilde") {
+	
 	$filepath = "..".$dir.$filename;
 	$sql = "UPDATE medlemmer SET foto = '".$filepath."' WHERE medlemsid = '".$medlemsid."' LIMIT 1";
 	mysql_query($sql);
+	echo "Oppdaterte bilde for " . $medlemsid . " til ". $filepath;
 }
 
 // slett filer som starter på medlemsid: rm 211-*
 
+echo "Prøver å lagre bilde til ".__DIR__ . $dir . $filename;
 if (\Flow\Basic::save( __DIR__ . $dir . $filename, $config, $request)) {
   echo "Hurray, file was saved in " . $dir . $filename;
 }
