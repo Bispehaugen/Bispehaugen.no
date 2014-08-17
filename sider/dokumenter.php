fe914ac63e24fffa7035c42ad05471b896ea7894
Anne-Siri Borander <as.borander@gmail.com>
2014-08-17 21:19:34 +0200

    lagt inn link til dokumenter/media på serveren slik at alt i alle fall er tilgjengelig

--- sider/dokumenter.php
+++ sider/dokumenter.php
@@ -1,2 +1,6 @@
 <h2>Dokumenter</h2>
-<p>Kommer ... :)</p>
+<p>Denne siden er ikke ferdig ennå, men her kan man midlertidig finne alle dokumentene og filene. Dessverre er det
+	ikke mulig å laste opp filer ennå, men det kommer etterhvert.</p>
+	
+	Dokumenter kan finnes <a href='http://bispehaugen.no/dokumenter2/'>her</a>.<br>
+	Mediafiler (lyd/film) kan finnes <a href='http://bispehaugen.no/media/'>her</a>.
