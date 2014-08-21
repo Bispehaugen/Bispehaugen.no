1317646ac1177bd5abb8d5e241606cf780880540
Anne-Siri Borander <as.borander@gmail.com>
2014-08-21 12:35:13 +0200

    forslag til tekst som også tar hensyn til at vi bruker e-post

--- index.php
+++ index.php
@@ -99,7 +99,7 @@ $innhold = ob_get_clean();
 	<?php if(!er_logget_inn() && erForside()) { ?>
 		<section class="login">
 			<div class="errors feilmelding">
-				Kunne ikke logge inn, brukernavn eller passord er feil. Kontakt webkom hvis dette fortsetter :)
+				Kunne ikke logge inn, e-post eller passord er feil. Husk at vi har begynt å bruke e-post i stedet for brukernavn. Kontakt webkom hvis dette fortsetter :)
 			</div>
 			<div class="login-box">
 				<h2 class="overskrift">Internsiden</h2>
