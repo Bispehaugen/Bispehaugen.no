7a698e6633da897a1f7d3caee2479de7edd78fd1
trond klakken <trond.klakken@bekk.no>
2015-03-02 09:37:25 +0100

    Flyttet dokumenter2 til dokumenter

--- sider/dokumenter.php
+++ sider/dokumenter.php
@@ -2,5 +2,5 @@
 <p>Denne siden er ikke ferdig ennå, men her kan man midlertidig finne alle dokumentene og filene. Dessverre er det
 	ikke mulig å laste opp filer ennå, men det kommer etterhvert.</p>
 	
-	<a href='http://bispehaugen.no/dokumenter2/'>Dokumenter kan finnes her</a>.<br><br>
+	<a href='http://bispehaugen.no/dokumenter/'>Dokumenter kan finnes her</a>.<br><br>
 	<a href='http://bispehaugen.no/media/'>Mediafiler kan finnes her</a>.
