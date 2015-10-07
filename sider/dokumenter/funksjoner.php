95c2e59b5da76addfa37bf3100f9e4ae15e070a8
Trond Klakken <trond@klakken.com>
2015-10-07 21:43:38 +0200

    Sortert ASC... ikke DESC :)

--- sider/dokumenter/funksjoner.php
+++ sider/dokumenter/funksjoner.php
@@ -7,12 +7,12 @@ function hent_mapper($ider, $hentUndermapper=false, $mappetype = null) {
 	if(!is_null($mappetype)) {
 		$sql .= " AND mappetype = ".$mappetype;
 	}
-	$sql .=" ORDER BY tittel DESC";
+	$sql .=" ORDER BY tittel ASC";
 	return hent_og_putt_inn_i_array($sql, $id_verdi=$id_verdi);
 }
 
 function hent_filer($mappeid) {
-	$sql="SELECT id, filnavn, tittel, beskrivelse, filtype, medlemsid, mappetype, tid FROM filer WHERE mappeid = ".intval(mysql_real_escape_string($mappeid)) . " ORDER BY tittel DESC";
+	$sql="SELECT id, filnavn, tittel, beskrivelse, filtype, medlemsid, mappetype, tid FROM filer WHERE mappeid = ".intval(mysql_real_escape_string($mappeid)) . " ORDER BY tittel ASC";
 	return hent_og_putt_inn_i_array($sql, $id_verdi="id");
 }
 
@@ -55,7 +55,7 @@ function sok_mapper($sokestreng, $mappetype = Mappetype::Dokumenter) {
 	foreach($delstrenger as $delstreng) {
 		$sql .= "AND tittel LIKE '%" . mysql_real_escape_string($delstreng) . "%'";
 	}
-	$sql .=" ORDER BY tittel DESC";
+	$sql .=" ORDER BY tittel ASC";
 	return hent_og_putt_inn_i_array($sql, $id_verdi="id");
 }
 
@@ -65,7 +65,7 @@ function sok_filer($sokestreng, $mappetype = Mappetype::Dokumenter) {
 	foreach($delstrenger as $delstreng) {
 		$sql .= "AND tittel LIKE '%" . mysql_real_escape_string($delstreng) . "%'";
 	}
-	$sql .=" ORDER BY tittel DESC";
+	$sql .=" ORDER BY tittel ASC";
 	return hent_og_putt_inn_i_array($sql, $id_verdi="id");
 }
 
