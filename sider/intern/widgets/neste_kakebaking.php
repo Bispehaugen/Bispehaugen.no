c2262767c81f13ac27778cf3a93a97822f18687c
Trond Klakken <trond@klakken.com>
2015-10-09 18:55:16 +0200

    Bare vis kakebaking hvis man finner en

--- sider/intern/widgets/neste_kakebaking.php
+++ sider/intern/widgets/neste_kakebaking.php
@@ -1,6 +1,8 @@
 <?php
 	// Neste kakebaking
 	$neste_kakebaking = neste_kakebaking();
+
+	if (!empty($neste_kakebaking)) {
 	$neste_kakebaking_tid = strtotime($neste_kakebaking['dato']." ".$neste_kakebaking['oppmoetetid']);
 
 ?>
@@ -14,13 +16,16 @@
 	</a>
 	
 	<p><i class="fa fa-calendar-o fa-fw"></i>
-		<?php echo strftime("%A", $neste_konsert_tid); ?>
-		<?php echo date("d.", $neste_konsert_tid); ?>
-		<?php echo strftime("%B", $neste_konsert_tid); ?>
-		, kl. <?php echo date("H:i", $neste_konsert_tid); ?></p>
+		<?php echo strftime("%A", $neste_kakebaking_tid); ?>
+		<?php echo date("d.", $neste_kakebaking_tid); ?>
+		<?php echo strftime("%B", $neste_kakebaking_tid); ?>
+		, kl. <?php echo date("H:i", $neste_kakebaking_tid); ?></p>
 	<p>
 		<a href="https://maps.google.com/maps?q=<?php echo $neste_konsert["sted"]; ?>">
 			<i class="fa fa-location-arrow fa-fw"></i><?php echo $neste_konsert["sted"]; ?>
 		</a>
 	</p>
 </section>
+
+<?php
+}
