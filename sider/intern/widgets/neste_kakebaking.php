d3f3d5b41117bb9f9b16f34c7180f52ec2da304c
Trond Klakken <trond@klakken.com>
2015-10-11 00:14:31 +0200

    Marker neste kakebaking når den begynner å nærme seg på internsiden

--- sider/intern/widgets/neste_kakebaking.php
+++ sider/intern/widgets/neste_kakebaking.php
@@ -1,12 +1,14 @@
 <?php
 	// Neste kakebaking
+	$neste_kakebaking_varsle_n_dager_for = 6;
+
 	$neste_kakebaking = neste_kakebaking();
 
 	if (!empty($neste_kakebaking)) {
 	$neste_kakebaking_tid = strtotime($neste_kakebaking['dato']." ".$neste_kakebaking['oppmoetetid']);
-
+	$neste_kakebaking_markert = $neste_kakebaking_tid - (86400 * $neste_kakebaking_varsle_n_dager_for) < time();
 ?>
-<section class="widget neste-kakebaking">
+<section class="widget neste-kakebaking<?php if($neste_kakebaking_markert) echo " markert" ?>">
 	
 	<a href='?side=aktiviteter/vis&arrid=<?php echo $neste_kakebaking["arrid"]; ?>'>
 		<h3>
