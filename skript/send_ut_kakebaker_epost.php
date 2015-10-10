7e42b9c500a688d194512a9b193c33556b299a9e
Trond Klakken <trond@klakken.com>
2015-10-10 23:25:08 +0200

    Send epost til kakebaker 4 dager før

--- skript/send_ut_kakebaker_epost.php
+++ skript/send_ut_kakebaker_epost.php
@@ -15,39 +15,38 @@ if ($tilkobling === false) {
 }
 $sekunder_i_ett_dogn = 86400; //24*60*60;
 
-$om_4_dager = time() + (4*$sekunder_i_ett_dogn);
-$neste_kakebaker_sql = "SELECT * FROM arrangement WHERE dato > NOW() and dato < ".$om_4_dager." ORDER BY dato LIMIT 1";
-echo $neste_kakebaker_sql;
+$om_4_dager = date('Y-m-d', time() + (4*$sekunder_i_ett_dogn)) . " 23:59:59";
+$neste_kakebaker_sql = "SELECT * FROM arrangement WHERE dato > NOW() and dato < '".$om_4_dager."' ORDER BY dato LIMIT 1";
 
 $arrangement = hent_og_putt_inn_i_array($neste_kakebaker_sql);
-print_r($arrangement);
 
 // Sjekk om arrangement allerede er varslet
 $allerede_varslet_sql = "SELECT COUNT(id) as antall FROM varsling WHERE arrid = " . $arrangement['arrid'] . " AND type = " . Varslingstype::Kakebaker;
 $allerede_varlset = hent_og_putt_inn_i_array($allerede_varslet_sql);
 
-
 if ($allerede_varlset['antall'] == 0 && !empty($arrangement)) {
 
 	$bruker = hent_brukerdata($arrangement['kakebaker']);
 	$tid = strtotime($arrangement['dato'] . " " . $arrangement['oppmoetetid']);
-	$antall_dager = round($tid - time() / $sekunder_i_ett_dogn);
 
-	$to = "tkthekiller@gmail.com"; //$bruker['epost'];
+	$antall_dager = round(($tid - time()) / $sekunder_i_ett_dogn);
+
+	$to = $bruker['email'];
 	$replyto = "styret@bispehaugen.no";
 	$subject = "Kakebaking - Bispehaugen.no";
 	$message = "
 Hei " . $bruker['fnavn'] . "!
 
-Det er du som skal bake kake om " . $antall_dager ." dager for \"" . $arrangement['tittel'] . "\", kl." . date("H:i d.m.Y") . ".
+Det er du som skal bake kake om " . $antall_dager ." dager for \"" . $arrangement['tittel'] . "\", kl. " . date("H:i d.m.Y", $tid) . ".
 Hvis det ikke passer må du selv sørge for å finne en stedfortreder.
+
+Les mer på http://bispehaugen.no/?side=aktiviteter/vis&arrid=".$arrangement['arrid']."
 
 Med vennlig hilsen
 Styret";
 
-	echo $message;
-	if(true) { //epost($to,$replyto,$subject,$message)) {
-		$slq_varling = "INSERT INTO varsling (arrid, type) VALUES (".$arrangement['arrid'].", " . Varslingstype::Kakebaker . ")";
-		mysql_query($slq_varling);
+	if(epost($to, $replyto, $subject, $message)) {
+		$sql_varling = "INSERT INTO varsling (arrid, type, tid) VALUES (".$arrangement['arrid'].", " . Varslingstype::Kakebaker . ", '".date("Y-m-d H:i:s")."')";
+		mysql_query($sql_varling);
 	}
 }
