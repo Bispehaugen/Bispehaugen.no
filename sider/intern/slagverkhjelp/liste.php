665e34b8050bb909f2e81e0133fece2cb553b626
Trond Klakken <trond@klakken.com>
2015-10-18 22:39:55 +0200

    Slagverkbæreoversikt: Ikke vis drag and drop for de som ikke kan redigere :)

--- sider/intern/slagverkhjelp/liste.php
+++ sider/intern/slagverkhjelp/liste.php
@@ -7,14 +7,26 @@ if (!er_logget_inn()) {
 	die();
 }
 
-function formater_gruppe($gruppeid, $medlemmer) {
-	$html .= "<section class='gruppe med-leder' data-gruppe-id='".$gruppeid."' data-drop-effect='all'>";
+function formater_gruppe($gruppeid, $medlemmer, $redigeringsmodus) {
+	$html .= "<section class='gruppe med-leder' ";
+	if($redigeringsmodus) {
+		$html .= "data-gruppe-id='".$gruppeid."' data-drop-effect='all'";
+	}
+	$html .= ">";
 	$html .=  "<h2>Gruppe " . $gruppeid . "</h2>";
 	$html .=  "<ul class='hjelpere'>";
 	foreach($medlemmer as $hjelper) {
-		$html .=  "<li class='hjelper' draggable='true' data-medlemsid='".$hjelper['medlemsid']."'>"
-		. $hjelper['fnavn'] . " " . $hjelper['enavn']
-		. (($hjelper['hengerfeste']==1 && tilgang_endre()) ? " <span class='hengerfeste' title='Har tilgang på bil med hengerfeste'>(H)</span>":"")
+		$html .=  "<li class='hjelper'";
+		if($redigeringsmodus) {
+			$html .= " draggable='true' data-medlemsid='".$hjelper['medlemsid']."'";
+		}
+		$html .= ">";
+		if($redigeringsmodus) {
+			$html .= $hjelper['fnavn'] . " " . $hjelper['enavn'];
+		} else {
+			$html .= brukerlenke($hjelper, Navnlengde::FulltNavn, false);
+		}
+		$html .= (($hjelper['hengerfeste']==1 && tilgang_endre()) ? " <span class='hengerfeste' title='Har tilgang på bil med hengerfeste'>(H)</span>":"")
 		. "</li>";
 	}
 	$html .=  "</ul>";
@@ -23,11 +35,14 @@ function formater_gruppe($gruppeid, $medlemmer) {
 }
 
 $grupper = hent_slagverkhjelp();
-$gruppeIder = range(1, max(array_keys($grupper))); // Lag gruppeIder for alle grupper mellom 1 og høyeste id
-$plasserteMedlemsIder = array_values($grupper);
-$uplasserteMedlemmer = array_filter(hent_medlemmer(), function($medlemsid) {
-    return !in_array($medlemsid, $plasserteMedlemsIder);
-}, ARRAY_FILTER_USE_KEY);
+$redigeringsmodus = false; //tilgang_endre();
+
+if ($redigeringsmodus) {
+	$gruppeIder = range(1, max(array_keys($grupper))); // Lag gruppeIder for alle grupper mellom 1 og høyeste id
+	$plasserteMedlemsIder = array_values($grupper);
+	$uplasserteMedlemmer = array_filter(hent_medlemmer(), function($medlemsid) {
+	    return !in_array($medlemsid, $plasserteMedlemsIder);
+	}, ARRAY_FILTER_USE_KEY);
 
 ?>
 <script type='text/javascript'>
@@ -35,34 +50,48 @@ $uplasserteMedlemmer = array_filter(hent_medlemmer(), function($medlemsid) {
 	var endredeBrukerSomErLeder = {};
 	var eksisterendeGrupper = [<?php echo join(',', $gruppeIder); ?>];
 </script>
+
+<?php } // $redigeringsmodus ?>
 <link rel="stylesheet" href="css/slagverkhjelp.css" type="text/css" />
-<article class='slagverkhjelp'>
+<article class='slagverkhjelp<?php echo $redigeringsmodus?"rediger":""; ?>'>
 
 <h1>Slagverkbæregrupper</h1>
 <p>Se <a href='?side=forside'>hovedsiden</a> for å se neste gang du skal bære og hvilken gruppe du hører til</p>
+
+<?php 
+if ($redigeringsmodus) {
+?>
 <p>Klikk og dra navn for å endre slagverkgruppe. Alle uten gruppe finner du i den nederste boksen.</p>
 <button class='button button-border lagre' disabled=disabled>Status: <i class='status-ikon fa'></i><span class='status'>Gjør dine endringer</span></button>
 
-<div class='clearfix'></div>
-<?php if (tilgang_endre()) { ?>
+<?php 
+} // $redigeringsmodus
+?>
 <p>
 	<span class='gruppeleder'>GL = Gruppeleder</span>
 	<span class='hengerfeste'>H = Har tilgang til bil med hengefeste</span>
 </p>
-<?php } ?>
-<section class='slagverkgrupper'>
+<div class='clearfix'></div>
 
+<section class='slagverkgrupper'>
 
 <?php
 
-foreach($gruppeIder as $gruppeid) {
-	if(in_array($gruppeid, $gruppeIder)) {
-		echo formater_gruppe($gruppeid, $grupper[$gruppeid]);
-	} else {
-		echo formater_gruppe($gruppeid, Array());
+if ($redigeringsmodus) {
+	foreach($gruppeIder as $gruppeid) {
+		if(in_array($gruppeid, $gruppeIder)) {
+			echo formater_gruppe($gruppeid, $grupper[$gruppeid], $redigeringsmodus=true);
+		} else {
+			echo formater_gruppe($gruppeid, Array(), $redigeringsmodus=true);
+		}
+	}
+} else {
+	foreach($grupper as $gruppeid => $gruppe) {
+		echo formater_gruppe($gruppeid, $grupper[$gruppeid], $redigeringsmodus=false);
 	}
 }
 
+if ($redigeringsmodus) {
 ?>
 	<section class='gruppe add-gruppe button'>
 		<i class='ikon fa fa-plus'></i>
@@ -261,3 +290,5 @@ window.addEventListener('load',function(){
 
 });
 </script>
+<?php
+} // $redigeringsmodus
