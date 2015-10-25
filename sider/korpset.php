6dd622845529fbb82c1a83c22af922fc2ba34084
Trond Klakken <trond@klakken.com>
2015-10-25 15:49:36 +0100

    La til info om nye dirigent

--- sider/korpset.php
+++ sider/korpset.php
@@ -8,12 +8,15 @@
     </p>
 
     <p>
-	    <img width="100" src="../bilder/medlemsfoto/Tomas_2.jpg" style="float:left;margin-right:8px" alt="Tomas Carstensen">
-	    Siden høsten 2000 har Tomas Carstensen vært vår faste dirigent. Han har utdannelse som trompetist
-	    og dirigent fra Musikkonservatoriet i Trondheim.
-    </p>
-
-    <p>
+	Jan Viggo Øwre har bachelor i utøvende tuba fra Universitetet i Stavanger og Universitetet i Bergen. 2009-2010 var han vernepliktig musiker i Forsvarets Musikkorps Vestlandet (nå Sjøforsvarets Musikkorps), og har siden 2010 vært ansatt i Alta Kulturskole som messinglærer og korpsdirigent. En av hovedoppgavene her har vært å dirigere skole- og voksenkorps i Alta.
+	</p>
+<p>
+I Alta tok han praktisk-pedagogisk utdanning ved Universitetet i Tromsø og skal nå begynne på master i musikkvitenskap ved NTNU.
+</p>
+<p>
+Tidligere har han dirigert korps i bergensområdet, og har ofte vært brukt som instruktør på seminarer i både skole- og voksenkorps. Som musiker har han vunnet NM to ganger og fått en andreplass i EM med Stavanger Brass Band. Han har også spilt med bl.a. Kleppe Musikklag, Sandefjord Musikkorps, Nanset Blåseensemble, Eikanger-Bjørsvik Musikklag og Bergen Filharmoniske Orkester.
+</p>
+ <p>
 	    Korpset har i mange år hevdet seg i toppen av norsk 1. divisjon i NM janitsjar (<a href="meritter.php">se resultater</a>). Under NM i 2006 vant 
 	    korpset 1. divisjon, og konkurrerte i elitedivisjonen i to år. I perioden 2009-2011 konkurrerte vi i 1. divisjon, og fra 2012 har korpset konkurrert i 2. divisjon
 	    Vår musikalske målsetting er å ha full Symphonic Band-besetning, og å være et ungdomskorps i ordets rette forstand. Bispehaugen ønsker ikke å bli plassert i en bestemt bås, men skal
