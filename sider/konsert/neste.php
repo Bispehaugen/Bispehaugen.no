70a4940dfb7b8871f66628658353fda70a377458
Sindre Stephansen <sindre@sindrestephansen.com>
2016-10-15 19:45:33 +0200

    La til to tidspunkter for høstkonserten. Det er en dårlig hack som må fjernes rett etterpå

--- sider/konsert/neste.php
+++ sider/konsert/neste.php
@@ -46,10 +46,20 @@ if(isset($konsert['normal_pris']) || isset($konsert['student_pris'])) {
     }
 }
 
+# fancyDato(kanskje($konsert, 'konsert_tid'), true)
 echo '
               <p class="ingress">'.$konsert['ingress'].'</p>
           </div>
-          '.fancyDato(kanskje($konsert, 'konsert_tid'), true).'
+        <time class="fancy-date" datetime="2016-11-05T15:00:00+01:00" title="lø. 05. nov. 2016 kl. 15.00 +0100">
+            <div class="boks">
+                <div class="weekday">lø.</div>
+                <div class="day">5.</div>
+                <div class="month">nov.</div>
+                <div class="year">2016</div>
+            </div>
+            <div class="time boks">kl. 15:00</div>
+            <div class="time boks">kl. 18:00</div>
+        </time>
         </article>
     ';
 
