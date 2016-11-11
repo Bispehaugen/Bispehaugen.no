0593e8fa555572f40ae913aaffde0b2a7e7efbd9
Sindre Stephansen <sindre@sindrestephansen.com>
2016-11-12 00:34:28 +0100

    Utvidet det redigerbare området av 'Om oss' på forsiden

--- sider/korpset.php
+++ sider/korpset.php
@@ -1,5 +1,2 @@
-<h2>Korpset</h2>
-    <p>
-	    <img width="100%" src="images/korpset.jpeg" style="float:right;margin-left:8px" alt="Fra NM for janitsjar">
-        <?php echo innhold("korpset"); ?>
-    <div class="clearfix"></div>
+<?php echo innhold("korpset"); ?>
+<div class="clearfix"></div>
