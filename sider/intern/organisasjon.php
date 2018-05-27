ab35486adae1ae95cc7d93070b49b2a9a41426ad
Sindre Stephansen <sindre@sindrestephansen.com>
2018-05-27 02:25:43 +0200

    Oppdater styret og komiteer

--- sider/intern/organisasjon.php
+++ sider/intern/organisasjon.php
@@ -1,12 +1,12 @@
 <?php
 	//funksjonalitet
-	
+
 	//henter ut alle komiteer
 	$komiteer = hent_komiteer();
-	
+
 	//henter ut info om medlemmer++ om valgte komité
     $valgtekomiteer = hent_styret();
-	
+
 	$ovelse_utfylt = false;
 	// Sjekker om du har sendt inn fraværsmelding
 	if (has_post('ovelse') && post('ovelse') != "") {
@@ -15,7 +15,7 @@
 		$bruker = innlogget_bruker();
 		$navn = $bruker['fnavn']." ".$bruker['enavn'];
 		$epost = $bruker['email'];
-		
+
 		$from = "From: Bispehaugen.no<ikke-svar@bispehaugen.no>";
 		$to = 'nestleder@bispehaugen.no';
 		$replyto = "Reply-To: $navn <$epost>";
@@ -24,19 +24,19 @@
 		$subject = "Fraværsmelding - $navn";
 
 		$message = "Fraværsmelding fra $navn\r\nØvelse: $ovelse\r\nGrunn:$grunn";
-		
+
 		$header="$from\r\n"."$replyto\r\n"."$realfrom";
 		if(!epost($to, $replyto, $subject, $message, $header)) {
 			$feil_under_sending_av_mail = true;
 		}
-		
+
 		$ovelse_utfylt = true;
 	}
 
 	//det som skrives ut på side
 
 	?>
-	
+
 <section class="praktisk">
 <?php echo innhold("praktisk"); ?>
 </section>
@@ -48,7 +48,7 @@
 <p>
 <b>Leder:</b> Siri Espe
 </p><p>
-<b>Nestleder:</b> Ane Agdestein
+<b>Nestleder:</b> Anders Johnsen
 </p><p>
 <b>Økonomiansvarlig:</b> Anne Margrethe Vik
 </p><p>
@@ -60,25 +60,26 @@
 </p><p>
 <b>Sponsor- og dugnadsansvarlig:</b> Kristian Thinn Solheim
 </p><p>
-<b>Besetningsansvarlig:</b> Guro Rafoss Kverneland
+<b>Besetningsansvarlig:</b> Mari Lomnes
 </p>
 
- 
+
 <h3>Webkomiteen:</h3>
 
 <p>Sekretær; Oda Elise Strandås (leder)</p>
 <p>Sindre Stephansen (medlem)</p>
-<p>Jenny Bakkerud (medlem)</p>
+<p>Simen Holmestad (medlem)</p>
 <br />
 <p>Komiteens arbeidsoppgaver omfavner i hovedsak å videreutvikle, drifte og oppdatere korpsets hjemmeside, facebookside og e-postlister. </p>
 
- 
+
 <h3>Økonomikomiteen:</h3>
 
 <p>Sponsor og dugnadsansvarlig; Kristian Thinn Solheim (leder)</p>
 <p>Økonomiansvarlig: Anne Margrethe Vik</p>
-<p>Lene Sevland (medlem)</p>
-<p>Merete Hvattum (medlem)</p>
+<p>Astrid Nordhus (medlem)</p>
+<p>Solveig Rivenes Lone (medlem)</p>
+<p>Birgitte Ellingsen (medlem)</p>
 <br />
 <p>
 Komiteen arbeider med å skaffe korpset inntekter. Dette innebærer blant annet å organisere dugnader og spilleoppdrag.</p>
@@ -87,9 +88,9 @@ Komiteen arbeider med å skaffe korpset inntekter. Dette innebærer blant annet 
 <h3>Musikkomiteen:</h3>
 
 <p>Musikkansvarlig; Silje Engeland (Musikalsk leder)</p>
-<p>Elise Andersen Solbakk (medlem)</p>
-<p>Solveig Rivenes Lone (medlem)</p>
-<p>Andreas K. L. Belboe (medlem)</p>
+<p>Silje Margrethe Jørgensen (medlem)</p>
+<p>Kine Bjørnbakk (medlem)</p>
+<p>Daniel Einskar Ellevsen (medlem)</p>
 <br />
 <p>Musikkomiteen skal fastsette korpsets repertoar og har ansvaret for korpsets noter, notearkiv og notedatabasen.</p>
 
@@ -97,38 +98,38 @@ Komiteen arbeider med å skaffe korpset inntekter. Dette innebærer blant annet 
 
 <h3>Arrangementskomiteen:</h3>
 
-<p>Nestleder; Ane Agdestein (leder)</p>
+<p>Nestleder; Anders Johnsen (leder)</p>
 <p>Musikkansvarlig: Silje Engeland</p>
-<p>Silje Margrethe Jørgensen (medlem)</p>
+<p>Arnhild Pedersen (medlem)</p>
+<p>Aleksander Styrmoe (medlem)</p>
 <p>Mari Flønes (medlem)</p>
-<p>Rasmus Høgberg (medlem)</p>
 <br />
 <p>Komiteen har ansvaret for det tekniske ved alle konserter korpset med særlig fokus på høstkonserten.</p>
 
 <h3>Besetningskomite:</h3>
-<p>Besetningsansvarlig; Guro Rafoss Kverneland (leder)</p>
-<p>Bente Skårholen Lomnes (medlem)</p>
-<p>Torbjørn Moi (medlem)</p>
+<p>Besetningsansvarlig; Mari Lomnes (leder)</p>
+<p>Ane Agdestein (medlem)</p>
+<p>Ingvild Haug Brekken (medlem)</p>
 <br />
 <p>Komiteen jobber med å ha kontroll på korpsets besetning til ulike konserter, arrangement og konkurranser, samt arrangere sosiale tilstelninger for korpsets medlemmer. </p>
 
 <h3>Valgkomiteen:</h3>
-<p>Markus Skorpen (leder)</p>
-<p>Bente Skårholen Lomnes (medlem)</p>
-<p>Arnhild Pedersen (medlem)</p>
-<p>Martin Okstad (vara)</p>
+<p>Arne Aarhus (leder)</p>
+<p>Silje Engeland (medlem)</p>
+<p>Solveig Gjerstad Neeraas (medlem)</p>
+<p>Markus Skorpen (vara)</p>
 <br />
 <p>Valgkomiteen skal innstille valgbare kandidater til tillitsverv.</p>
 
- 
+
 
 <h3>Revisjonskomiteen:</h3>
-<p>Martin Okstad (leder)</p>
-<p>Mari Lomnes (medlem)</p>
-<p>Karianne Hansen (vara)</p>
+<p>Markus Skorpen (leder)</p>
+<p>Joachim Spange (medlem)</p>
+<p>Bente Skårholen Lomnes (vara)</p>
 <br />
 <p>Revisorene skal gjennomgå regnskapet for hver årsmøteperiode og legge fram beretning for årsmøtet.</p>
-		
+
 <?php
 /*<p><a href='http://org.ntnu.no/buk/filer/dokumenter/Vedlegg_A2.pdf'>Instruksen for komitéer</a></p>
 			//skriver ut alle komiteene med link til komitevisning
@@ -153,6 +154,6 @@ Komiteen arbeider med å skaffe korpset inntekter. Dette innebærer blant annet 
 				echo "</section>";
 
 			}
-			
+
 	echo "</section>";
 	*/
